e03bfb83dc9cea50acfde6e3bc5943edb640c596
bcrenato <bcrenato@gmail.com>
2025-06-25 17:39:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase (substitua pelas suas credenciais)
-const supabaseUrl = 'https://seu-projeto.supabase.co';
-const supabaseKey = 'sua-chave-publica';
+const supabaseUrl = 'https://gtpsdstjfdygmdkgevml.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cHNkc3RqZmR5Z21ka2dldm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4Nzk4MTgsImV4cCI6MjA2NjQ1NTgxOH0.xj78Xz4rOgQ4Xrn_feLakJLbUb-4rGMxU9Ul-rb5yec';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Variáveis globais
