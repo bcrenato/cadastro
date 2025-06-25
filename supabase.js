72b6b9a384af1002ee665494572fe67285807332
bcrenato <bcrenato@gmail.com>
2025-06-25 19:30:22 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 const { createClient } = supabase;
-const supabaseUrl = 'https://SEU-PROJETO.supabase.co';
-const supabaseKey = 'chave-publica';
+const supabaseUrl = 'https://gtpsdstjfdygmdkgevml.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cHNkc3RqZmR5Z21ka2dldm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4Nzk4MTgsImV4cCI6MjA2NjQ1NTgxOH0.xj78Xz4rOgQ4Xrn_feLakJLbUb-4rGMxU9Ul-rb5yec';
 const supabase = createClient(supabaseUrl, supabaseKey);
