98eb673a9d41353b813b26943244d2370244c0a8
bcrenato <bcrenato@gmail.com>
2025-06-25 19:16:06 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const { createClient } = supabase;
+const supabaseUrl = 'https://SEU-PROJETO.supabase.co';
+const supabaseKey = 'chave-publica';
+const supabase = createClient(supabaseUrl, supabaseKey);
