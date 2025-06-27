e91a7f632877a5fbe6eaf7a3494e7ed94ec3017e
bcrenato <bcrenato@gmail.com>
2025-06-26 22:42:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-let timeout;
+window.buscarCEP = function () {
+  const cep = document.getElementById('cep').value.replace(/\D/g, '');
 
-function buscarCEP() {
-  clearTimeout(timeout);
-  timeout = setTimeout(() => {
-    const cep = document.getElementById('cep').value.replace(/\D/g, '');
-
-    if (cep.length === 8) {
-      fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => response.json())
-        .then(data => {
-          if (data.erro) {
-            document.getElementById('endereco').value = 'CEP não encontrado';
-          } else {
-            const enderecoCompleto = `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
-            document.getElementById('endereco').value = enderecoCompleto;
-          }
-        })
-        .catch(error => {
-          console.error('Erro ao buscar o CEP:', error);
-          document.getElementById('endereco').value = 'Erro ao buscar';
-        });
-    }
-  }, 500);
-}
+  if (cep.length === 8) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.erro) {
+          document.getElementById('endereco').value = 'CEP não encontrado';
+        } else {
+          const enderecoCompleto = `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
+          document.getElementById('endereco').value = enderecoCompleto;
+        }
+      })
+      .catch(error => {
+        console.error('Erro ao buscar o CEP:', error);
+        document.getElementById('endereco').value = 'Erro ao buscar';
+      });
+  } else {
+    document.getElementById('endereco').value = ''; // Limpa se for incompleto
+  }
+};
