9b4ee4628f0a3fbda22aaa6f06d65ffa5d0172d0
bcrenato <bcrenato@gmail.com>
2025-06-27 18:51:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-s
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 // ✅ Exportando firebaseConfig para import por login.html
-export const firebaseConfig = {
-  apiKey: "AIzaSyCv6pzl34tyPTARtQeA-xU",
+const firebaseConfig = {
+  apiKey: "AIzaSyCv6pzl34tyPTARtGxV6g2AJfkrtQeA-xU",
   authDomain: "cadastro-igreja-23042.firebaseapp.com",
   databaseURL: "https://cadastro-igreja-23042-default-rtdb.firebaseio.com",
   projectId: "cadastro-igreja-23042",
+  storageBucket: "cadastro-igreja-23042.firebasestorage.app",
   messagingSenderId: "977906864836",
   appId: "1:977906864836:web:1a21a29f4b941ac5aeaf91"
 };
