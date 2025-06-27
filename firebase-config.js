096b53aeb77ad25fcf65a5845afd0c7d4330c6ec
bcrenato <bcrenato@gmail.com>
2025-06-27 19:23:54 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,20 +4,16 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-// ✅ Exportando firebaseConfig para import por login.html
 const firebaseConfig = {
   apiKey: "AIzaSyCv6pzl34tyPTARtGxV6g2AJfkrtQeA-xU",
   authDomain: "cadastro-igreja-23042.firebaseapp.com",
   databaseURL: "https://cadastro-igreja-23042-default-rtdb.firebaseio.com",
   projectId: "cadastro-igreja-23042",
-  storageBucket: "cadastro-igreja-23042.firebasestorage.app",
   messagingSenderId: "977906864836",
   appId: "1:977906864836:web:1a21a29f4b941ac5aeaf91"
 };
 
-// Inicializa o Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database(); // Agora usando Realtime Database
-
-export { app, auth, db, firebaseConfig };
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
