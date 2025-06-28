763d2ff9ee768f315a5f538f8724b694ae273d92
bcrenato <bcrenato@gmail.com>
2025-06-28 16:53:20 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+import { 
+  initializeAuth, 
+  indexedDBLocalPersistence,
+  browserLocalPersistence 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv6pzl34tyPTARtGxV6g2AJfkrtQeA-xU",
+  authDomain: "cadastro-igreja-23042.firebaseapp.com",
+  databaseURL: "https://cadastro-igreja-23042-default-rtdb.firebaseio.com",
+  projectId: "cadastro-igreja-23042",
+  messagingSenderId: "977906864836",
+  appId: "1:977906864836:web:1a21a29f4b941ac5aeaf91"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: indexedDBLocalPersistence
+});
+
+export const db = getDatabase(app);
+export const storage = getStorage(app);
+export { auth };
