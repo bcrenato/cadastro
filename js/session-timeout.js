5048ca6c35521106716236afe72e0bd3da3f798b
bcrenato <bcrenato@gmail.com>
2025-07-25 12:44:46 -0300

    Update session-timeout.js

--- js/session-timeout.js
+++ js/session-timeout.js
@@ -1,4 +1,4 @@
-// js/session-timeout.js
+// /js/js/session-timeout.js
 import { auth } from '../firebase-config.js';
 
 const timeoutMillis = 2 * 60 * 1000;
@@ -11,6 +11,10 @@ function resetTimer() {
 
 function logout() {
     console.log("Sessão expirada por inatividade.");
+
+    // Salva a URL atual antes de deslogar
+    localStorage.setItem("lastVisitedPage", window.location.href);
+
     auth.signOut().then(() => {
         window.location.href = "/cadastro/login.html";
     }).catch((error) => {
@@ -19,8 +23,22 @@ function logout() {
     });
 }
 
+// Detecta atividades do usuário
 ['click', 'mousemove', 'keydown', 'scroll', 'touchstart'].forEach(evt => {
     document.addEventListener(evt, resetTimer, false);
 });
 
 resetTimer();
+
+// Redirecionar para última página salva após login, se houver
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        const lastPage = localStorage.getItem("lastVisitedPage");
+
+        if (lastPage && lastPage !== window.location.href && !window.location.href.includes("/cadastro/login.html")) {
+            // Limpa e redireciona
+            localStorage.removeItem("lastVisitedPage");
+            window.location.href = lastPage;
+        }
+    }
+});
